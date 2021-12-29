f3cd0e46a3c6e76fe3c23367b905a26d88af7f0e
Santhosh Nageshwar <santhosh_rao@intuit.com>
2021-12-29 14:03:10 -0500

    Step13 - setup context

---
+++ pages/logout.js
@@ -0,0 +1,10 @@
+import { useEffect } from "react";
+import { useUser } from "../context/user";
+
+const Logout = () => {
+  const { logout } = useUser();
+  useEffect(logout, []);
+  return (<p>Logging Out</p>)
+}
+
+export default Logout;
