cfc6b679f1c45fb545ded29b460a80c89e9ffc5a
Santhosh Nageshwar <santhosh_rao@intuit.com>
2021-12-29 17:17:20 -0500

    17-Shared Nav

--- pages/[id].js
+++ pages/[id].js
@@ -1,12 +1,27 @@
+import { useEffect, useState } from "react";
 import { supabase } from "../utils/supabase";
-
+import Video from 'react-player';
 
 const LessonDetails = ({ lesson }) => {
-  console.log(lesson);
+  const [videoUrl, setVideoUrl] = useState(null)
+
+  const getPremiumContent = async () => {
+    const { data } = await supabase
+      .from('premium_content')
+      .select('video_url')
+      .eq('id', lesson.id)
+      .single();
+
+    setVideoUrl(data?.video_url)
+  }
+
+  useEffect(getPremiumContent, [])
+
   return (
     <div className="w-full max-w-3xl mx-auto py-16 px-8">
       <h1 className="text-3xl mb-6">{lesson.title}</h1>
-      <p>{lesson.description}</p>
+      <p className="mb-8">{lesson.description}</p>
+      {videoUrl && <Video url={videoUrl} controls={true} width={'100%'} />}
     </div>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
 import '../styles/globals.css'
 import UserProvider from '../context/user';
+import Nav from '../components/nav';
 function MyApp({ Component, pageProps }) {
   return (
     <UserProvider>
+      <Nav />
       <Component {...pageProps} />
     </UserProvider>
   )
